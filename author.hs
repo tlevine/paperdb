d398bd6c611deb7691744e54eab0ca2fd4982aa6
Thomas Levine <occurrence@thomaslevine.com>
2013-01-13 11:26:44 -0600

    aoeu

--- author.hs
+++ author.hs
@@ -2,14 +2,17 @@ import qualified Data.Map as Map
 --import qualified Data.Aeson
 --import qualified Text.JSON
 
+import Data.String.Utils
+
 author :: String -> [Map.Map String a]
-author jsonstring = map sql rows
+author jsonstring = "CREATE TABLE tablename (\n" ++ schema ++ "\n);"
   where
     rows = Data.Aeson.decode jsonstring
+    schema = join ",\n" $ map sql rows
 
 sql :: Map.Map String a -> String
 sql row
-  | rowType == "point" = "[" ++ (lookup "xlab" row) ++ "] FLOAT NOT NULL," ++ "[" ++ (lookup "ylab" row) ++ "] FLOAT NOT NULL"
+  | rowType == "point" = "[" ++ (lookup "xlab" row) ++ "] FLOAT NOT NULL,\n" ++ "[" ++ (lookup "ylab" row) ++ "] FLOAT NOT NULL"
   | rowType == "image" = "[" ++ (lookup "lab" row) ++ "] TEXT NOT NULL" -- Base64
   | rowType == "text" =  "[" ++ (lookup "lab" row) ++ "] TEXT NOT NULL"
   where
